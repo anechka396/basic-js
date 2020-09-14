54f072be18a1c5df3471f1b7cf25f0b3d57481f6
Anna Yakubenko <ayakubenko@atypicaldigital.com>
2020-09-14 13:46:31 +0300

    feat: add transform-array solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,27 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if(!Array.isArray(arr)) throw new Error();
+  if(arr.length == 1) return arr;
+  
+  var transformed_arr = [];
+  for(var i =0; i < arr.length; i++) {
+	  if(arr[i] === '--discard-next') {
+		  i++;
+	  } else if (arr[i] === '--discard-prev' && arr[i - 2] != '--discard-next') {
+		  if(transformed_arr.length > 0) transformed_arr.pop();
+	  } else if(arr[i] === '--double-next') {
+		  if(i+1 < arr.length) transformed_arr.push(arr[i+1]);
+	  } else if(arr[i] === '--double-prev' && arr[i - 2] !== '--discard-next') {
+		  if(transformed_arr.length > 0) {
+			var last = transformed_arr.pop();
+			transformed_arr.push(last);
+			transformed_arr.push(last);
+		  };
+	  } else if(arr[i] != '--discard-next' && arr[i] != '--discard-prev' && arr[i] != '--double-next' && arr[i] != '--double-prev' && arr[i] !== undefined) {
+		  transformed_arr.push(arr[i]);
+	  }
+  }
+  
+  return transformed_arr;
 };
